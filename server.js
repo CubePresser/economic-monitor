ad27c3d4d0ebefebb87fa268dc863959704e7df2
jonesjonathan <jonesjon@oregonstate.edu>
2019-07-09 19:38:12 -0700

    Transfer work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const controllers = require('./controllers');
 
 const port = process.env.PORT || 8000;
 
@@ -10,14 +11,16 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 
-app.use('*', (err, req, res, next) => {
+app.use('/', controllers);
+
+app.use((err, req, res) => {
     console.error(err);
     res.status(500).send({
         error: "Server error. Try again later."
     });
 });
 
-app.use('*', (req, res, next) => {
+app.use((req, res) => {
     res.status(404).send({
         error: "Requested resource " + req.originalUrl + " does not exist"
     });
